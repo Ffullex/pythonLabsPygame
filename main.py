c359448e16d0a9ce741fa4ae43005a7d999a7aa2
tomanovskiyyv <tomanovskiyyv@mer.ci.nsu.ru>
2021-02-22 10:33:54 +0700

    some ref

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
 running = True
 
 while running:
@@ -30,24 +38,20 @@ while running:
     screen.blit(surf, surf_center)
     pygame.display.flip()
     for event in pygame.event.get():
-        #if event.type == K_UP:
 
-        #elif event.type == K_DOWN:
-
-        #elif event.type == K_LEFT:
-
-        #elif event.type == K_RIGHT:
 
         if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
+            if event.type == K_UP:
+                y += 10
+            elif event.type == K_DOWN:
+                y -= 10
+            elif event.type == K_LEFT:
+                x += 10
+            elif event.type == K_RIGHT:
+                x += 10
+            elif event.key == K_ESCAPE:
                 running = False
         elif event.type == QUIT:
             running = False
 
-    class Player(pygame.sprite.Sprite):
-        def __init__(self):
-                    super(Player, self).__init__()
 
-                    self.surf = pygame.Surface((75, 25))
-                    self.surf.fill((255, 255, 255))
-                    self.rect = self.surf.get_rect()
